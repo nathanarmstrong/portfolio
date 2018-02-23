aa3bf758c57d4b0a324bf8f570cc8ebe1c2c2e7e
nathanarmstrong <nathan.armstrong21@gmail.com>
2018-02-23 14:45:36 -0700

    added image to banners, description of self, what i know icon change, show live projects, add linkdin

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ jQuery(function($){
 });
 
 jQuery(function($){
-  $('#projects .project-laptop').click(function(ev){
+  $('#myCarousel a').click(function(ev){
     var href = $(this).attr('href');
     if (/^#/.exec(href)) {
       ev.preventDefault();
       $('body, html').animate({'scroll-top': $(href).position().top - 80});
+    }
+    $('.navbar-toggle').click();
+  });
+});
+
+jQuery(function($){
+  $('#projects .project-laptop').click(function(ev){
+    var href = $(this).attr('href');
+    if (/^#/.exec(href)) {
+      ev.preventDefault();
+      $('body, html').animate({'scroll-top': $(href).position().top + 30});
     }
     $('.navbar-toggle').click();
   });
